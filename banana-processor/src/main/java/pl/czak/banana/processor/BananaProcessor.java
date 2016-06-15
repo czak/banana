dd672b4fad83922ccfbed52f10ec193f9be2ac31
Łukasz Adamczak <lukasz@czak.pl>
2016-06-15 14:16:37 +0200

    Implement a basic banana-processor module

---
+++ banana-processor/src/main/java/pl/czak/banana/processor/BananaProcessor.java
@@ -0,0 +1,36 @@
+package pl.czak.banana.processor;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+import pl.czak.banana.BindView;
+
+public class BananaProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Messager messager = processingEnv.getMessager();
+        for (Element e : roundEnv.getElementsAnnotatedWith(BindView.class)) {
+            String msg = String.format("Banana processing: %s", e.toString());
+            messager.printMessage(Diagnostic.Kind.NOTE, msg);
+        }
+        return false;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(BindView.class.getCanonicalName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+}
